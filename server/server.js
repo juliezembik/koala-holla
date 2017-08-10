4ea3590df0323e09e424c211923c4fcc41300deb
Luke Schlangen <lukeschlangen@gmail.com>
2017-08-10 16:06:43 -0500

    Updating with express.static

--- server/server.js
+++ server/server.js
@@ -5,12 +5,7 @@ var path = require( 'path' );
 var port = process.env.PORT || 5000;
 
 app.use(bodyParser.urlencoded({extended: true}));
-
-// Serve back static files by default
-app.get('/*', function(req, res) {
-  var file = req.params[0] || '/views/index.html';
-  res.sendFile(path.join(__dirname, '/public/', file));
-});
+app.use(express.static('server/public'));
 
 // Start listening for requests on a specific port
 app.listen(port, function(){
