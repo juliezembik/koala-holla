fec278f928fc12e4a63887d16a61b9ab8355241b
Christopher Black <chris@primeacademy.io>
2017-10-19 10:20:16 -0500

    Removed unused module - path.

--- server/server.js
+++ server/server.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
-var path = require( 'path' );
 var port = process.env.PORT || 5000;
 
 app.use(bodyParser.urlencoded({extended: true}));
