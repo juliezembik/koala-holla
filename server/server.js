a2562b25c4a488365b13ff6c303863c0690282ea
Dane Smith <doctorhowser@gmail.com>
2018-10-10 10:37:01 -0500

    update server, router, es6

--- server/server.js
+++ server/server.js
@@ -1,12 +1,16 @@
-var express = require('express');
-var app = express();
-var bodyParser = require('body-parser');
-var port = process.env.PORT || 5000;
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const PORT = process.env.PORT || 5000;
+const koalaRouter = require('./routes/koala.router')
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('server/public'));
 
+// ROUTES
+app.use('/koalas', koalaRouter)
+
 // Start listening for requests on a specific port
-app.listen(port, function(){
-  console.log('listening on port', port);
+app.listen(PORT, () => {
+  console.log('listening on port', PORT);
 });
