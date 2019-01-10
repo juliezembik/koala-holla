4e385ebaa356da9b0cbb3379a5d405b0e9b793f3
Peng Xue Vang <pxvang33@gmail.com>
2019-01-10 14:03:14 -0600

    pull

--- server/routes/koala.router.js
+++ server/routes/koala.router.js
@@ -2,10 +2,7 @@ const express = require('express');
 const koalaRouter = express.Router();
 const pg = require('pg');
 
-<<<<<<< HEAD
 
-=======
->>>>>>> fefdfcd6d51f10129c4a55afd577aabe6c6f0f9d
 // DB CONNECTION
 const Pool = pg.Pool;
 const pool = new Pool ({
@@ -13,12 +10,12 @@ const pool = new Pool ({
     host: 'localhost',
     port: 5432,
     max: 10,
-    idleTimeoutMillis: 10000
+    idleTimeMillis: 10000
 });
 
 
 // GET
-koalaRouter.get('/', (req, res) => {
+router.get('/', (req, res) => {
     let queryText = `SELECT * FROM "inventory";`;
     pool.query(queryText).then((result) => {
         console.log(result);
