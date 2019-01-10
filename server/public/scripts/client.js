070b01450ee76024340c1cd2476f3847d63c39cf
Kye Berkeland <kyeberkeland@yahoo.com>
2019-01-10 14:50:40 -0600

    Add the ready to transfer button

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -10,7 +10,8 @@ $( document ).ready( function(){
 }); // end doc ready
 
 function setupClickListeners() {
-  $( '#addButton' ).on( 'click', saveKoala); 
+  $( '#addButton' ).on( 'click', saveKoala);
+  $( '#viewKoalas').on( 'click', '.transfer', transferKoala);
 }
 
 function getKoalas(){
@@ -23,7 +24,7 @@ function getKoalas(){
     url: '/koalas'
   }).then(function(response){
     console.log('in GET response', response);
-    listOfKoala = response;
+    let listOfKoala = response;
     for(koala of listOfKoala){
      // append each koala to the DOM
       $('#viewKoalas').append(`<tr>
@@ -32,6 +33,8 @@ function getKoalas(){
                                     <td>${koala.age}</td>
                                     <td>${koala.ready_to_transfer}</td>
                                     <td>${koala.notes}</td>
+                                    <td><button type="button" class="transfer" data-koalaready=${koala.id}>Ready to transfer</button></td>
+                                    <td><button type="button" class="delete" data-koaladelete=${koala.id}>Delete</button></td>
       </tr>`)
     }
   }).catch(function(error){
@@ -46,19 +49,37 @@ function saveKoala(){
   // variable holding the koala data from input fields
   let koalasToServer = { name: $('#nameIn').val(),
                          age: $('#ageIn').val(),
-                         gender: $('genderIn').val(),
-                         transfer: $('readyForTransferIn').val(),
+                         gender: $('#genderIn').val(),
+                         ready_to_transfer: $('#readyForTransferIn').val(),
                          notes: $('#notesIn').val()
                        }
   $.ajax({ method: 'POST',
            url: '/koalas',
            data: koalasToServer
-        }).then((result) =>{
-          console.log(result);
-          getKoalas();
-        }).catch((errorMessage) => {
-          // alert telling the user no record was created
-          alert(`There was a problem with adding your koala`);
-          console.log(`Error message: ${errorMessage}`);
-        });
+  }).then((result) =>{
+    console.log(result);
+    getKoalas();
+  }).catch((errorMessage) => {
+    // alert telling the user no record was created
+    alert(`There was a problem with adding your koala`);
+    console.log(`Error message in POST: ${errorMessage}`);
+  });
 } // end saveKoalla
+
+// function for changing the transfer koala value
+function transferKoala() {
+  // get the id from transfer button
+  console.log('In transferKoala');
+  const koalaData = $(this).data('koalaready');
+  console.log(koalaData);
+  $.ajax({ method: 'PUT',
+           url: `/koalas/${koalaData}`
+  }).then((result) => {
+    console.log(result);
+    getKoalas();
+  }).catch((errorMessage) => {
+    // alert telling the user no record was created
+    alert(`Koala transfer did not go through!`);
+    console.log(`Error message in PUT: ${errorMessage}`);
+  })
+} // end transferKoala
