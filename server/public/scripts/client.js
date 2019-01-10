e619772f722c571bfd4b0440283838e200fd9637
Kye Berkeland <kyeberkeland@yahoo.com>
2019-01-10 13:03:41 -0600

    Add ajax PUSH to getKoalas function

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -19,7 +19,24 @@ function getKoalas(){
   
 } // end getKoalas
 
+// Post request for adding a koala to the database
 function saveKoala(){
   // ajax call to server to get koalas
- 
-}
+  // variable holding the koala data from input fields
+  let koallaToServer = { name: $('#nameIn').val(),
+                         age: $('#ageIn').val(),
+                         gender: $('genderIn').val(),
+                         transfer: $('readyForTransferIn').val(),
+                         notes: $('#notesIn').val()
+                       }
+  $.ajax({ method: 'POST',
+           url: '/koala',
+           data: koallaToServer
+        }).then((result) =>{
+          console.log(result);
+        }).catch((errorMessage) => {
+          // alert telling the user no record was created
+          alert(`There was a problem with adding your koala`);
+          console.log(`Error message: ${errorMessage}`);
+        });
+} // end saveKoalla
